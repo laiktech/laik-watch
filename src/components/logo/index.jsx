10acd23f70185d45cddb809b0691d3fa839b1987
Estrella-BF <gracielabenitesf@gmail.com>
2025-12-18 22:30:08 -0500

    fix: logo

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom';
 import ButtonBase from '@mui/material/ButtonBase';
 
 // project imports
-import Logo from './LogoMain';
-import LogoIcon from './LogoIcon';
 import { APP_DEFAULT_PATH } from 'config';
+
+// assets
+import logo from 'assets/logos/logo-text.svg';
 
 // ==============================|| MAIN LOGO ||============================== //
 
 export default function LogoSection({ reverse, isIcon, sx, to }) {
   return (
-    <ButtonBase disableRipple component={Link} to={to || APP_DEFAULT_PATH} sx={sx} aria-label="Logo">
-      {isIcon ? <LogoIcon /> : <Logo reverse={reverse} />}
+    <ButtonBase disableRipple component={Link} to={to || APP_DEFAULT_PATH} sx={{...sx, width: "10em"}} aria-label="Logo">
+      <img src={ logo } />
     </ButtonBase>
   );
 }
