59de13c8b53bb0637987e40cb41ddc9d3f86a7c1
Estrella-BF <gracielabenitesf@gmail.com>
2025-12-19 13:24:51 -0500

    footer

--- src/layout/Dashboard/Footer.jsx
+++ src/layout/Dashboard/Footer.jsx
@@ -7,19 +7,19 @@ export default function Footer() {
   return (
     <Stack direction="row" sx={{ alignItems: 'center', justifyContent: 'space-between', p: '24px 16px 0px', mt: 'auto' }}>
       <Typography variant="caption">
-        &copy; All rights reserved{' '}
-        <Link href="https://codedthemes.com/" target="_blank" underline="hover">
-          CodedThemes
+        &copy; Todos los derechos reservados {' '}
+        <Link href="" target="_blank" underline="hover">
+          Laiktech
         </Link>
       </Typography>
       <Stack direction="row" sx={{ gap: 1.5, alignItems: 'center', justifyContent: 'space-between' }}>
-        <Link href="https://codedthemes.com/about-us/" target="_blank" variant="caption" color="text.primary">
+        <Link href="" target="_blank" variant="caption" color="text.primary">
           About us
         </Link>
-        <Link href="https://mui.com/legal/privacy/" target="_blank" variant="caption" color="text.primary">
+        <Link href="" target="_blank" variant="caption" color="text.primary">
           Privacy
         </Link>
-        <Link href="https://mui.com/store/terms/" target="_blank" variant="caption" color="text.primary">
+        <Link href="" target="_blank" variant="caption" color="text.primary">
           Terms
         </Link>
       </Stack>
