f5a7dfa6ad2690b40d7120a4afb6e52a46af87a5
Estrella-BF <gracielabenitesf@gmail.com>
2025-12-19 10:34:21 -0500

    feat: delete Support page

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -2,12 +2,11 @@
 import dashboard from './dashboard';
 import pages from './page';
 import utilities from './utilities';
-import support from './support';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages, utilities, support]
+  items: [dashboard, pages, utilities] // support
 };
 
 export default menuItems;
